f1158d0f28d34cf5453de0dbe4c88eafe2d0e145
amplifypath <info@amplify-path.com>
2025-06-09 00:21:01 +0200

    Update cokie.js

--- cokie.js
+++ cokie.js
@@ -4,16 +4,23 @@ puppeteer.use(StealthPlugin());
 const fs = require('fs');
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
-  await page.goto('https://twitter.com/login');
+    const browser = await puppeteer.launch({
+    headless: false,
+    args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        // (and any others you need, e.g. `--user-data-dir=...`)
+    ]
+    });
+    const page = await browser.newPage();
+    await page.goto('https://twitter.com/login');
 
-  console.log('Please log in manually...');
-  await new Promise(resolve => setTimeout(resolve, 60000)); // Wait 60 seconds
+    console.log('Please log in manually...');
+    await new Promise(resolve => setTimeout(resolve, 60000)); // Wait 60 seconds
 
-  const cookies = await page.cookies();
-  fs.writeFileSync('twitter_cookies.json', JSON.stringify(cookies, null, 2));
-  console.log('Cookies saved.');
+    const cookies = await page.cookies();
+    fs.writeFileSync('twitter_cookies.json', JSON.stringify(cookies, null, 2));
+    console.log('Cookies saved.');
 
-  await browser.close();
+    await browser.close();
 })();
